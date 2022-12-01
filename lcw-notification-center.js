a11063c39f53bcf55a0abb74f7e10091aa1901ca
irembuz <irembuz7@gmail.com>
2022-12-01 16:28:28 +0300

    Local storage conditions added.

--- lcw-notification-center.js
+++ lcw-notification-center.js
@@ -7,9 +7,29 @@
     document.getElementsByTagName("head")[0].appendChild(jQueryScript);
   }
 
+  function showModal() {
+    if (isProductPage() && hasThreeProducts()) {
+      console.log("Show Modal");
+    } else {
+      console.log("Do not Show Modal");
+    }
+  }
+
+  function isProductPage() {
+    const productPageIndicator = "urun";
+    return document.location.pathname.includes(productPageIndicator);
+  }
+
+  function hasThreeProducts() {
+    const notificationProducts = localStorage.getItem("notificationProducts")
+      ? JSON.parse(localStorage.getItem("notificationProducts"))
+      : null;
+    return notificationProducts && notificationProducts.length === 3;
+  }
+
   addJQuery();
 
   setTimeout(() => {
-    console.log("jQuery has been added.");
+    showModal();
   }, 1000);
 })();
